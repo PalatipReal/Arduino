6a3c426855d432ba9aaf5a946d7cfac98dacbc9c
PalatipReal <sungsilver3@gmail.com>
2019-09-06 09:29:46 +0700

    example

--- Led_ESP32/sketch_mar30a/sketch_mar30a.ino
+++ Led_ESP32/sketch_mar30a/sketch_mar30a.ino
@@ -1,9 +1,11 @@
 #include <LED.h>
-  LED led(2);
+  LED led(2);         // set pin led
 void setup() {
   
 }
 
 void loop() {
-  led.Toggle(500);
+  led.Toggle(500);    // Toggle State Led
+//   led.On();
+//   led.Off();
 }
