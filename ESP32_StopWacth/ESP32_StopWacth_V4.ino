f4bbba17306fbc6b11d9782ebaa0254e61b46911
PalatipReal <sungsilver3@gmail.com>
2020-03-09 02:32:17 +0700

    Add files via upload

---
+++ ESP32_StopWacth/ESP32_StopWacth_V4.ino
@@ -0,0 +1,175 @@
+//************ include LiquidCrystal_I2C ************
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+
+//************ Set Interruppin ************
+#define interruptPin1   33   // For Start,Stop InterruptTimer
+#define interruptPin2   12   // For Reset InterrupTimer
+int CountInterrupt33  = 0 ; 
+int CountInterrupt12  = 0 ;
+bool StateTimer       = false;    // false = Stop , true = Start...
+bool StateDisplay      = false;
+
+//************ Set Time ************
+int Hour              = 0 ;
+int Min               = 0 ;
+int Sec               = 0 ;
+int MiliSec           = 0 ;
+
+//************ Set Debouncing_time ************
+long debouncing_time1 = 300; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros1;
+
+long debouncing_time2 = 300; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros2;
+
+hw_timer_t * timer = NULL;
+
+void LCDDisplay(){
+  if(Hour < 10){
+    lcd.setCursor(3,0);
+    lcd.print("0");      
+    lcd.setCursor(4,0);
+    lcd.print(Hour);      // Set Cursor For Hour 
+    }
+  if(Hour >= 10){
+    lcd.setCursor(3,0);
+    lcd.print(Hour);      // Set Cursor For Hour 
+    }
+  lcd.setCursor(5,0);
+  lcd.print(":");
+
+  if(Min < 10){
+    lcd.setCursor(6,0);
+    lcd.print("0");      
+    lcd.setCursor(7,0);
+    lcd.print(Min);      // Set Cursor For Min 
+    }
+  if(Min >= 10){
+    lcd.setCursor(6,0);
+    lcd.print(Min);      // Set Cursor For Min 
+    }
+  lcd.setCursor(8,0);
+  lcd.print(":");
+
+  if(Sec < 10){
+    lcd.setCursor(9,0);
+    lcd.print("0");      
+    lcd.setCursor(10,0);
+    lcd.print(Sec);      // Set Cursor For Sec 
+    }
+  if(Sec >= 10){
+    lcd.setCursor(9,0);
+    lcd.print(Sec);      // Set Cursor For Sec 
+    }
+  lcd.setCursor(11,0);
+  lcd.print(":");
+
+  if(MiliSec < 10){
+    lcd.setCursor(12,0);
+    lcd.print("0");      
+    lcd.setCursor(13,0);
+    lcd.print(MiliSec);      // Set Cursor For Sec 
+    }
+  if(MiliSec >= 10){
+    lcd.setCursor(12,0);
+    lcd.print(MiliSec);      // Set Cursor For Sec 
+    }
+}
+void IRAM_ATTR onTimer(){
+  MiliSec++;
+  if(MiliSec == 100){
+    MiliSec = 0;
+    Sec++;
+    if(Sec == 60){
+      Sec = 0;
+      Min++;
+      if(Min == 60){
+        Min = 0;
+        Hour++;
+        if(Hour == 99){
+          Hour = 0;
+        }
+      }
+    }
+  }
+}
+void Display_Task(void *p){ 
+  while(1) {
+      LCDDisplay();
+      Serial.print("Hour :");
+      Serial.print(Hour);
+      Serial.print(" Min :");
+      Serial.print(Min);
+      Serial.print(" Sec :");
+      Serial.print(Sec);
+      Serial.print(" MiliSec :");
+      Serial.print(MiliSec);
+      Serial.println("");
+      vTaskDelay(10);
+      }
+  }
+
+void setup() {
+  pinMode(interruptPin1,INPUT_PULLUP);
+  pinMode(interruptPin2,INPUT_PULLUP);
+  
+  timer = timerBegin(0, 80, true);                            // timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
+  timerAttachInterrupt(timer, &onTimer, true);                // edge (not level) triggered 
+  timerAlarmWrite(timer, 10000, true);                        // 1000000 * 1 us = 1 s, autoreload true
+  timerAlarmEnable(timer);                                    // SetTimer Disable,Enable    
+  timerStop(timer);                                           // SetTimer Disable,Enable   
+    
+  attachInterrupt(interruptPin1,debounceInterrupt1,FALLING);  // Set InrerruptPin1 
+  attachInterrupt(interruptPin2,debounceInterrupt2,FALLING);  // Set InrerruptPin1
+  
+  lcd.begin();                                                // initialize the LCD
+  lcd.backlight();                                            // Turn on the blacklight and print a message.
+  lcd.clear();
+  
+  Serial.begin(9600);
+  Serial.println("Test Interrupt_And_Timer ESP32...");
+  LCDDisplay();                                               // Display Timer
+  Serial.println("Try Push Button To Start");
+  xTaskCreate(&Display_Task, "Display_Task", 2048, NULL, 1, NULL);
+} 
+
+void debounceInterrupt1() {
+  if((long)(micros() - last_micros1) >= debouncing_time1 * 1000) {
+    Handleinterrupt1();
+    last_micros1 = micros();
+  }
+}
+
+void debounceInterrupt2() {
+  if((long)(micros() - last_micros2) >= debouncing_time2 * 1000) {
+    Handleinterrupt2();
+    last_micros2 = micros();
+  }
+}
+
+void Handleinterrupt1() {
+  if(StateTimer == false){            // if Timer is Stop
+    timerStart(timer);                // Enable Timer
+    StateTimer = true ;               // Timer is Start
+    }
+  else{
+    timerStop(timer);  // Disable Timer
+    StateTimer = false;
+    }
+
+}
+void Handleinterrupt2() {
+    timerStop(timer);             // Disable Timer
+    StateTimer  = 0 ;
+    Hour        = 0 ;
+    Min         = 0 ;
+    Sec         = 0 ;
+    MiliSec     = 0 ;
+    StateTimer  = false ;                    // Reset Timer
+}
+
+void loop() {
+
+}
